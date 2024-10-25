d36c7c1d8bcafe031f3c65714fcb070cf887129f
David Dias <1211415@isep.ipp.pt>
2024-10-25 05:31:28 +0100

    Solução do problema de sincronização de diagnósticos com conjugação de sintomas

---
+++ Backend/src/main/java/model/Ignorar.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Ignorar {
+    private int ID;
+
+    public Ignorar(int ID) {
+        this.ID = ID;
+    }
+
+    public int getID() {
+        return ID;
+    }
+}
