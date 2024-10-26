81e1b29045dceb30864b28e18b02b9c781d7ddcc
David Dias <1211415@isep.ipp.pt>
2024-10-26 05:30:18 +0100

    Correção de encoding entre o drools e o java. (32/128 rules)

--- Backend/src/main/java/fabrica/FabricaQuestoes.java
+++ Backend/src/main/java/fabrica/FabricaQuestoes.java
@@ -1,6 +1,7 @@
 package fabrica;
 
 import java.util.Collection;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import org.kie.api.runtime.ClassObjectFilter;
@@ -20,50 +21,68 @@ public class FabricaQuestoes {
     public static DiagnosticoDTO diagnostico;
 
     public static boolean answer(String sintomaString, List<String> possiveisValores, String valor, boolean multiselect) throws InterruptedException {
-        boolean evidenciaVerificada = false;
-        Collection<Sintoma> sintomas = (Collection<Sintoma>) DemoApplication.ksn.getObjects(new ClassObjectFilter(Sintoma.class));
-        for (Sintoma s : sintomas) {
-            if (s.getEvidencia().compareTo(sintomaString) == 0) {
-                if(s.getValor().compareTo(valor) == 0){
-                    How.adicionarSintomaHistorico(s);
-                    return true;
+
+        try {
+            String sintomaStrUTF8 = new String(sintomaString.getBytes(), "UTF-8");
+            String valorUTF8 = new String(valor.getBytes(), "UTF-8");
+            // map all values of possiveisValores to utf8 strings and store in a new list
+            List<String> possiveisValoresUTF8 = possiveisValores.stream().map(s -> {
+                try {
+                    return new String(s.getBytes(), "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                    return s;
                 }
-                evidenciaVerificada = true;
+            }).toList();
+
+            boolean evidenciaVerificada = false;
+            Collection<Sintoma> sintomas = (Collection<Sintoma>) DemoApplication.ksn.getObjects(new ClassObjectFilter(Sintoma.class));
+            for (Sintoma s : sintomas) {
+                if (s.getEvidencia().compareTo(sintomaStrUTF8) == 0) {
+                    if(s.getValor().compareTo(valorUTF8) == 0){
+                        How.adicionarSintomaHistorico(s);
+                        return true;
+                    }
+                    evidenciaVerificada = true;
+                }
             }
-        }
-        if(evidenciaVerificada){
+            if(evidenciaVerificada){
+                return false;
+            }
+            questao = new PerguntaDTO(sintomaStrUTF8, possiveisValoresUTF8, multiselect);
+            novaQuestao = true;
+
+            DemoApplication.lockPergunta.lock();
+            try{
+                DemoApplication.conditionPergunta.signal();
+            } finally {
+                DemoApplication.lockPergunta.unlock();
+            }
+            
+
+            DemoApplication.lockResposta.lock();
+            try{
+                DemoApplication.conditionResposta.await();
+            } finally {
+                DemoApplication.lockResposta.unlock();
+            }
+
+            String resposta = FabricaQuestoes.resposta;
+            String[] respostas = resposta.split(",");
+            boolean respostaValida = false;
+            for (String r : respostas) {
+                Sintoma s = new Sintoma(sintomaStrUTF8, possiveisValoresUTF8, r);
+                DemoApplication.ksn.insert(s);
+                if(r.compareTo(valorUTF8) == 0) {
+                    How.adicionarSintomaHistorico(s);
+                    respostaValida = true;
+                }
+            }
+            return respostaValida;
+        } catch (Exception e) {
+            e.printStackTrace();
             return false;
         }
-        questao = new PerguntaDTO(sintomaString, possiveisValores, multiselect);
-        novaQuestao = true;
-
-        DemoApplication.lockPergunta.lock();
-        try{
-            DemoApplication.conditionPergunta.signal();
-        } finally {
-            DemoApplication.lockPergunta.unlock();
-        }
-        
-
-        DemoApplication.lockResposta.lock();
-        try{
-            DemoApplication.conditionResposta.await();
-        } finally {
-            DemoApplication.lockResposta.unlock();
-        }
-
-        String resposta = FabricaQuestoes.resposta;
-        String[] respostas = resposta.split(",");
-        boolean respostaValida = false;
-        for (String r : respostas) {
-            Sintoma s = new Sintoma(sintomaString, possiveisValores, r);
-            DemoApplication.ksn.insert(s);
-            if(r.compareTo(valor) == 0) {
-                How.adicionarSintomaHistorico(s);
-                respostaValida = true;
-            }
-        }
-        return respostaValida;
     }
 
     public static void darDiagnostico() {
